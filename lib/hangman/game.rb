2b593d3e48cef44e07e43114bce2a0315cfbbd5a
Rafael Tejado <rtejado90@gmail.com>
2015-10-16 11:54:16 +0200

    Add method to select a word

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -2,11 +2,23 @@ module Hangman
   class Game
     def initialize(players)
       @players = players
-      @current_player, @other_player = players.shuffle
+
     end
 
     def play
       puts "Let's play hangman!"
+      select_guessing_player
+      puts "#{@evil_player.name} has been selected to add a word. Please #{@guessing_player.name}, don't look at the screen"
+      add_guessing_word
+      puts @guessing_word
+    end
+
+    def select_guessing_player
+      @guessing_player, @evil_player = @players.shuffle
+    end
+
+    def add_guessing_word
+      @guessing_word = @evil_player.add_guessing_word
     end
 
   end
--- lib/hangman/player.rb
+++ lib/hangman/player.rb
@@ -1,11 +1,15 @@
 module Hangman
   class Player
+    attr_reader :word
     def initialize(name)
       @name= name
       
     end
 
-    
+    def add_guessing_word
+      @word = gets.chomp
+    end
+
 
 
   end
