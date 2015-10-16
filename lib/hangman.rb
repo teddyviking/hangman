4173c21596397762630a16ba13faedbd82ac5fd1
Rafael Tejado <rtejado90@gmail.com>
2015-10-16 10:29:39 +0200

    Create Player class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,3 +3,5 @@ require "hangman/version"
 module Hangman
   # Your code goes here...
 end
+
+require_relative "./tic_tac_toe/player.rb"
---
+++ lib/hangman/player.rb
@@ -0,0 +1,13 @@
+module Hangman
+  class Player
+    def initialize(name)
+      @name= name
+      
+    end
+
+    
+
+
+  end
+
+end
